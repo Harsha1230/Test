e6de5b25a3eb7934705afb1c67bb65f28a2d07b5
Harsha1230 <53749070+Harsha1230@users.noreply.github.com>
2023-04-09 13:03:33 +0530

    changed code

--- src/api/DataServices/Controllers/WeatherForecastController.cs
+++ src/api/DataServices/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DataServices.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracin", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
